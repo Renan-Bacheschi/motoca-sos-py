b3cff7ce74983b41f96ebb080c815b9bfb84d3c7
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2026-02-02 22:02:16 -0300

    refactor: pk em email estava errado, adicionando index para consultas rapidas

--- app/schemas/usuario.py
+++ app/schemas/usuario.py
@@ -6,6 +6,6 @@ class Usuario(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     nome: str
     sobrenome: str
-    email: EmailStr = Field(unique=True, primary_key=True)
+    email: EmailStr = Field(unique=True, index=True )
     senha: str
 
